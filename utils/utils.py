75c2c991c1c217838dd7e4b103e6c73224288307
Bikas Chaudhary Tharu <vcbikash123@gmail.com>
2026-02-01 15:07:17 +0000

    added filter on text

--- utils/utils.py
+++ utils/utils.py
@@ -1,4 +1,5 @@
 from pypdf import PdfReader
+import re
 
 def extract_text_from_pdf(file_path):
     """
@@ -17,3 +18,24 @@ def extract_text_from_pdf(file_path):
             text += page.extract_text() + "\n"
     
     return text.strip()
+
+def preprocess_text(text):
+    """
+    Preprocesses the input text by converting it to lowercase and removing extra spaces.
+
+    Args:
+        text (str): The input text to preprocess.
+    Returns:
+        str: The preprocessed text.
+    """
+    # Convert to lowercase
+    text = text.lower()
+    text = re.sub(r"<.*?>", "", text) # remove HTML tags
+    text = re.sub(r"http\S+", "", text) # remove URLs
+    text = re.sub(r"(?:\.\s*){2,}", " ", text)  # remove .... and . . .
+    text = re.sub(r"[^a-z\s.,!?']", "", text) # keep only letters and basic punctuation
+    text = re.sub(r"\s+", " ", text).strip() 
+    # Remove extra spaces
+    text = ' '.join(text.split())
+
+    return text
